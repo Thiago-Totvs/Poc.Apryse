30b2ef5da0a7635d9dce9c9d6def555f5aef3a9f
Thiago-Totvs <thiago.mmelo@totvs.com.br>
2024-02-20 10:36:46 -0300

    New features testes: Merge Files + Add header Hash - OK

--- Poc.Apryse/Controllers/ApryseTestController.cs
+++ Poc.Apryse/Controllers/ApryseTestController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using pdftron.PDF;
 using pdftron.SDF;
 using Poc.Apryse.Requests;
+using static pdftron.PDF.Stamper;
 
 namespace AprysePoc.Controllers
 {
@@ -33,23 +34,19 @@ namespace AprysePoc.Controllers
         [HttpPost("open")]
         public IActionResult OpenFile(IFormFile formFile)
         {
-
-            byte[] byte_buffer = formFile.GetBytesFromFormFile();
-            IntPtr buffer = System.Runtime.InteropServices.Marshal.AllocHGlobal(byte_buffer.Length);
-
             try
             {
-                System.Runtime.InteropServices.Marshal.Copy(byte_buffer, 0, buffer, byte_buffer.Length);
-                var doc = new PDFDoc(byte_buffer, byte_buffer.Length);
+                var doc = new PDFDoc(formFile.GetBytesFromFormFile(), formFile.GetBytesFromFormFile().Length);
+                //Open using bytes and bytes lenght from formFile
 
-                if (!doc.InitSecurityHandler())
+                if (!doc.InitSecurityHandler()) //Verify if is ecrypted
                     return new BadRequestObjectResult("ERROR! Document is encrypted");
 
                 var bytesResult = doc.Save(SDFDoc.SaveOptions.e_linearized);
 
                 doc.Close();
 
-                return new OkObjectResult(bytesResult);
+                return new OkObjectResult(bytesResult); //Return bytes
 
             }
             catch (Exception ex)
@@ -66,32 +63,71 @@ namespace AprysePoc.Controllers
         {
             try
             {
-                var resultMergedDocument = new PDFDoc();
+                var resultMergedDocument = new PDFDoc(); //Empty Doc
 
                 foreach (var file in request.ArquivosPdf)
                 {
                     using var document = new PDFDoc(file.GetBytesFromFormFile(), file.GetBytesFromFormFile().Length);
+                    //Open using bytes and bytes lenght from formFile
 
-                    if (!document.InitSecurityHandler())
+                    if (!document.InitSecurityHandler()) //Verify if is ecrypted
                         return new BadRequestObjectResult("ERROR! Document is encrypted");
 
-                    for (int i = 1; i <= document.GetPageCount(); i++)
-                    {
-                        var page = document.GetPage(i);
-
-                        resultMergedDocument.PageInsert(resultMergedDocument.GetPageIterator(), page);
-                    }
+                    resultMergedDocument.InsertPages(resultMergedDocument.GetPageCount() + 1, 
+                            document, 1, document.GetPageCount(), PDFDoc.InsertFlag.e_none);
+                    //InsertPages Parameters:
+                    //Berfore x Pages
+                    //Source document
+                    //Source start page
+                    //Source last page
+                    //InsertFlag -> BookMarks or none
                 }
 
-                var bytesResult = resultMergedDocument.Save(SDFDoc.SaveOptions.e_linearized);
+                resultMergedDocument.Save($"./ArquivosGeradosLocalTest/{request.NomeArquivo}.pdf", SDFDoc.SaveOptions.e_incremental);
+                //Save local to check changes
 
-                resultMergedDocument.Save($"./ArquivosGeradosLocalTest/{request.NomeArquivo}.pdf", SDFDoc.SaveOptions.e_linearized);
-
-                return new OkObjectResult(bytesResult);
+                return new OkObjectResult(resultMergedDocument.Save(SDFDoc.SaveOptions.e_incremental)); //Return bytes
             }
             catch(Exception ex)
             {
                 Console.WriteLine($"**LOCAL CONSOLE** Unhandled exception while Merging Files: {ex.Message}");
+            }
+
+            return new OkResult();
+        }
+
+
+        [HttpPost("addhash")]
+        public IActionResult AddHash(IFormFile formFile)
+        {
+            try
+            {
+                var doc = new PDFDoc(formFile.GetBytesFromFormFile(), formFile.GetBytesFromFormFile().Length);
+
+                if (!doc.InitSecurityHandler()) //Verify if is ecrypted
+                    return new BadRequestObjectResult("ERROR! Document is encrypted");
+
+                var stamper = new Stamper(SizeType.e_relative_scale, 0.55, 0.55); //Define text size
+
+                stamper.SetPosition(-0.20, 0.48, true); //true -> percentage
+                                                        //false -> pixel
+
+                //Using default aligment cant add margin
+                //stamper.SetAlignment(HorizontalAlignment.e_horizontal_left, VerticalAlignment.e_vertical_top);
+
+                stamper.SetFont(Font.Create(doc, Font.StandardType1Font.e_courier, true)); //Can change Font
+                stamper.SetFontColor(new ColorPt(0, 0, 0, 0)); //Color black
+
+                var hash = "CÓDIGO: D9-60-6A-33-B5-56-F0-11-90-9C-42-22-25-7D-40-C6-9F-E5-BB-54";
+                stamper.StampText(doc, hash, new PageSet(1, doc.GetPageCount())); //PageSet define range pages
+
+                doc.Save("./teste.pdf", SDFDoc.SaveOptions.e_incremental); //Save local to check changes
+
+                return new OkObjectResult(doc.Save(SDFDoc.SaveOptions.e_incremental)); //Return bytes
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"**LOCAL CONSOLE** Unhandled exception while Adding Hash to File: {ex.Message}");
             }
 
             return new OkResult();
